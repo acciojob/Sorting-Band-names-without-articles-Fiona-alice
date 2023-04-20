23ed77843ba802be9a2be633182c68b0b95427ff
Fiona-alice <120317574+Fiona-alice@users.noreply.github.com>
2023-04-20 15:37:32 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 //your code here
 
+function strip(article) {
+  return article.replace(/^(a |an |the )/i, '').trim();
+}
+
+const sortedBands = bands.sort((a, b) => strip(a) > strip(b) ? 1 : -1);
+
+const ul = document.querySelector('#bands');
+
+sortedBands.forEach(band => {
+  const li = document.createElement('li');
+  li.textContent = band;
+  ul.appendChild(li);
+});
